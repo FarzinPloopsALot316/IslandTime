29c04689583968ecf8f7d425184d096a89cb1412
Farzin Riham <friham07@Gmail.com>
2024-05-19 16:02:15 -0400

    intro done

--- src/Day1Frame.java
+++ src/Day1Frame.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 public class Day1Frame {
-    private Day1Panel panel1;
+    private Day1Panel panel;
 
     public Day1Frame () {
 
@@ -9,13 +9,13 @@ public class Day1Frame {
         frame.setSize(900, 600); // 540 height of image + 40 for window menu bar
         frame.setLocationRelativeTo(null); // auto-centers frame in screen
 
-        panel1 = new Day1Panel(frame);
-        frame.add(panel1);
+        panel = new Day1Panel(frame);
+        frame.add(panel);
 
         frame.setVisible(true);
     }
 
     public void run() {
-        panel1.repaint();
+        panel.repaint();
     }
 }
