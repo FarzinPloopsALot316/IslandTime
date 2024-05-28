a5b614e6e5dda188dd4ed6ad97daa7a87f954d9b
BT_1N3_07 <no@bths.edu>
2024-05-28 08:42:31 -0400

    wood bkehjhhiwewui

--- src/Wood.java
+++ src/Wood.java
@@ -4,5 +4,26 @@ public class Wood {
     private int x;
     private int y;
 
+    public Wood (int width, int height, int x, int y) {
+        this.width =  width;
+        this.height = height;
+        this.x = x;
+        this.y =y;
+    }
 
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
 }
