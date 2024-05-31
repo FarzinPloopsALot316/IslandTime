26cfe6bc35c4a34402b26a0743014c987223a7fb
BT_1N3_07 <no@bths.edu>
2024-05-31 09:29:34 -0400

    HELP ME

--- src/Day2Panel.java
+++ src/Day2Panel.java
@@ -1,8 +1,19 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Day2Panel extends JPanel {
-    JFrame enclosingFrame;
+    private JFrame enclosingFrame;
+    private BufferedImage background;
+
     public Day2Panel (JFrame frame) {
         enclosingFrame = frame;
+        try {
+            background = ImageIO.read(new File("src/islandBeach.jpg"));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
